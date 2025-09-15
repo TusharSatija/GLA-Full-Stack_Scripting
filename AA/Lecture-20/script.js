21cac87ef7959c0a895a1c8710da7dcd3c43097e
TusharSatija <tusharsatija737@gmail.com>
2025-09-15 21:28:49 +0530

    css

---
+++ AA/Lecture-20/script.js
@@ -0,0 +1,21 @@
+// let name1=prompt("Enter your name");
+// console.log(name1);
+
+let fruits=["apple","mango","kiwi","Banana","orange"];
+let fav=prompt("enter fav fruit");
+
+console.log(fruits.includes(fav));
+// let flag=false;
+// for(let i=0;i<fruits.length;i++)
+// {
+//     if(fav.toLowerCase()==fruits[i])
+//     {
+//         flag=true;
+//         break;
+//     }
+// }
+
+// if(flag)
+//     alert("fav fruit avail");
+// else
+//     alert("fav fruit unavail");
---
+++ AC/Lecture-14/Script.js
@@ -0,0 +1,19 @@
+// let name1=prompt("enter your name");
+// console.log(name1);
+let menu=["panner butter masala","Samose","chole bhature","butter roti","chicken" ,"dal","icecream"];
+let inp=prompt("enter your fav food");
+
+console.log(menu.includes(inp));
+// let flag=false;
+// for(let i=0;i<menu.length;i++)
+// {
+//     if(inp.toLowerCase()==menu[i])
+//     {
+//         flag=true;      
+//         break;
+//     }
+// }
+// if(flag)
+//     alert("food avail");
+// else
+//     alert("food unavil");
